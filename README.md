8153e22fb203303690f467bd374123786b70d6d0
T.H <h.turuta@leihauoli.com>
2017-04-11 23:18:47 +0900

    commit first

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # javascript-face-authentication
-do facial recognition from an image in javascript
+It's a demo.
+You can do facial recognition from an image in javascript.
+
+## Description
+
+
+## Requirement
+- If you wanna use library of "Face Detection", you must ignore cross-origin.
+
+## Usage
+
+1. Usage
+2. Usage
+3. Usage
+
+## Installation
+
+
+
+## Anything Else
+
+AnythingAnythingAnything
+AnythingAnythingAnything
+AnythingAnythingAnything
+
+## Author
+
+## License
---
+++ js/faceAuthentication.js
@@ -0,0 +1,59 @@
+var FACE = FACE || {};
+
+FACE.CHANGE = {
+ CLASS: 'border',
+
+ init:function(){
+	this.setParameters();
+	this.bindEvent();
+ },
+
+ setParameters:function(){
+	this.$changeButton = $('.jsc-change-btn');
+	this.$imagId = $('#face-image');
+	this.$border = $('.' + this.CLASS);
+ },
+
+ bindEvent:function(){
+	var self = this;
+	this.$changeButton.click(function(e) {
+	 e.preventDefault();
+	 self.addAuthBorder();
+	});
+ },
+
+ addAuthBorder:function(){
+	var self = this;
+	this.$border.remove();
+
+	/* faceDetectionで画像にある顔を探す */
+	this.$imagId.faceDetection({
+		complete: function (faces) { //成功時
+				self.setBorderParams(faces);
+		},
+	 error:function (code, message) { //失敗時
+		alert('Error: ' + message);
+	 }
+	});
+ },
+
+	/* 顔認識がわかるボーダーを付与する */
+	setBorderParams:function(faces){
+		if(faces.length == 0){ return }
+
+		for (var i = 0; i < faces.length; i++) {
+				$('<div>', { 'class':this.CLASS,
+						'css': {
+								'left': faces[i].x * faces[i].scaleX + 'px',
+								'top': faces[i].y * faces[i].scaleY + 'px',
+								'width': faces[i].width  * faces[i].scaleX + 'px',
+								'height': faces[i].height * faces[i].scaleY + 'px'
+						}
+				}).insertAfter(this.$imagId);
+		}
+	}
+}
+
+$(function(){
+ FACE.CHANGE.init();
+});
